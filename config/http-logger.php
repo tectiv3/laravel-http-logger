4d38cb40a6eecd5e314b3647100be136ad26ebf9
tectiv3 <git@getpanda.me>
2022-11-08 12:15:18 +0900

    Update http-logger.php

--- config/http-logger.php
+++ config/http-logger.php
@@ -6,7 +6,7 @@ return [
      * The log profile which determines whether a request should be logged.
      * It should implement `LogProfile`.
      */
-    'log_profile' => \Spatie\HttpLogger\LogNonGetRequests::class,
+    'log_profile' => env('HTTPLOGGER_LOGGER_CLASS', \Spatie\HttpLogger\LogNonGetRequests::class),
 
     /*
      * The log writer used to write the request to a log.
@@ -25,7 +25,9 @@ return [
     /*
      * The log response is an options to decide log response or not
      */
-    'log_response' => true,
+    'log_response' => env('HTTPLOGGER_RESPONSE', true),,
+    'log_response_body' => env('HTTPLOGGER_RESPONSE_BODY', false),
+    'log_request_body' => env('HTTPLOGGER_REQUEST_BODY', false),
 
     /*
      * Log only the methods configured here
